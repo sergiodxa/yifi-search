bb3fae929e2e16622366537c1667fe777335eac3
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-06-12 17:25:01 -0300

    route /search added to routes.jsx

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Router from 'react-router';
 import App from '../components/app';
 import Home from '../views/home';
+import Search from '../views/search';
 
 export default (
   <Router.Route path="/" handler={ App }>
     <Router.DefaultRoute name="home" handler={ Home } />
+    <Router.Route name="search" handler={ Search } />
   </Router.Route>
 );
