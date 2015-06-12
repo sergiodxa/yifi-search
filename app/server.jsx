5eccaa2773c3ff59d2c63ce90b10a89f6035a25d
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-06-12 11:16:03 -0300

    basic sever created

---
+++ app/server.jsx
@@ -0,0 +1,24 @@
+import express from 'express';
+import engine  from 'react-engine';
+import path    from 'path';
+
+const app  = express();
+const port = Number(process.env.PORT || 3000);
+
+app.engine('.jsx', engine.create.server({
+  reactRoutes: 'node_modules/routes/index.jsx'
+}));
+
+app.set('views', path.join(__dirname, 'node_modules/views'));
+app.set('view engine', 'jsx');
+app.set('view', engine.expressView);
+
+app.get('/', (req, res) => {
+  const data = {
+    title: 'YIFI Search'
+  };
+
+  res.render(req.url, data);
+});
+
+app.listen(port, () => console.log(`App running on port ${port}`));
