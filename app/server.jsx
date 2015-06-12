ea073c19d53d06a128554a07a79ec5181e073814
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-06-12 17:24:00 -0300

    /search route in server.jsx and request-promise dependencie

--- app/server.jsx
+++ app/server.jsx
@@ -1,6 +1,7 @@
 import express from 'express';
 import engine from 'react-engine';
 import path from 'path';
+import request from 'request-promise';
 
 const app = express();
 const port = Number(process.env.PORT || 3000);
@@ -13,9 +14,23 @@ app.set('views', path.join(__dirname, 'node_modules/views'));
 app.set('view engine', 'jsx');
 app.set('view', engine.expressView);
 
-app.get('/', (req, res) => {
+app.get('/', async function HomeController(req, res) {
   const data = {
     title: 'YIFI Search'
+  };
+
+  res.render(req.url, data);
+});
+
+app.get('/search', async function SearchController(req, res) {
+  const query = req.query.query;
+  const url = `https://yts.to/api/v2/list_movies.json?query_term=${query}`;
+
+  const data = {
+    title: 'YIFI Search',
+    query,
+    url,
+    data: await request(url)
   };
 
   res.render(req.url, data);
