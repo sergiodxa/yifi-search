72c1c07f4c32f773464ad8c32709dd351b8e742a
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-06-28 01:39:35 -0300

    aplicadas algunas optimizaciones

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,15 @@ import babelify from 'babelify';
 import browserify from 'browserify';
 import buffer from 'vinyl-buffer';
 import eslint from 'gulp-eslint';
+import gcmq from 'gulp-group-css-media-queries';
 import gulp from 'gulp';
 import jadeify from 'jadeify';
 import minifyCSS from 'gulp-minify-css';
+import minifyHTML from 'gulp-minify-html';
 import rename from 'gulp-rename';
 import source from 'vinyl-source-stream';
 import stylus from 'gulp-stylus';
+import styleInject from 'gulp-style-inject';
 import uglify from 'gulp-uglify';
 
 const config = {
@@ -24,11 +27,13 @@ const paths = {
     js    : './src/es6/app.js',
     stylus: './src/stylus/**/*.styl',
     styles: './src/stylus/main.styl',
-    jade  : './src/jade/**/*.jade'
+    jade  : './src/jade/**/*.jade',
+    html  : './src/html/index.html'
   },
   build: {
     js    : './build/js/',
-    css   : './build/css/'
+    css   : './build/css/',
+    html  : './'
   }
 }
 
@@ -57,15 +62,24 @@ gulp.task('build:css', () => {
   gulp.src(paths.src.styles)
     .pipe(stylus())
     .pipe(rename('style.css'))
+    .pipe(gcmq())
     .pipe(minifyCSS())
     .pipe(gulp.dest(paths.build.css))
 });
 
-gulp.task('watch', () => {
-  gulp.watch([paths.src.eslint, paths.src.jade], ['build:js']);
-  gulp.watch([paths.src.stylus], ['build:css'])
+gulp.task('build:html', ['build:css'], () => {
+  gulp.src(paths.src.html)
+    .pipe(styleInject())
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(paths.build.html))
 });
 
-gulp.task('build', ['build:css', 'build:js']);
+gulp.task('watch', () => {
+  gulp.watch([paths.src.eslint, paths.src.jade], ['build:js']);
+  gulp.watch([paths.src.stylus], ['build:css', 'build:html']);
+  gulp.watch([paths.src.html], ['build:html']);
+});
+
+gulp.task('build', ['build:css', 'build:html', 'build:js']);
 
 gulp.task('default', ['build', 'watch']);
